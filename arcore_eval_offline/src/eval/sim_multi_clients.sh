a87d8b61ea320fca5c7a9488b8ed9792ab576adf
covislab <samuelyuyy@gmail.com>
2023-12-05 02:56:44 +0800

    Modify sim_multi_clients.sh usage

--- arcore_eval_offline/src/eval/sim_multi_clients.sh
+++ arcore_eval_offline/src/eval/sim_multi_clients.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 model_name=${1:-"help"}
-usage="usage: ./sim_multi_clients.sh model_name [path/to/sim_test_ids.txt] [n_clients] [server_address] [server_port] [path/to/outputs]"
+usage="usage: ./sim_multi_clients.sh model_name [n_clients] [path/to/sim_test_ids.txt] [server_address] [server_port] [path/to/outputs]"
 
 if [[ "$model_name" =~ help|--help|-h ]]
 then
     echo $usage
     exit
 else
-    sim_test_ids_txt=${2:-"sim_test_ids.txt"}
+    sim_test_ids_txt=${3:-"sim_test_ids.txt"}
     readarray -t test_ids < $sim_test_ids_txt
-    n_clients=${3:-"${#test_ids[@]}"}
+    n_clients=${2:-"${#test_ids[@]}"}
     server_address=${4:-"140.113.195.248"}
     server_port=${5:-"9999"}
     out=${6:-"out/CollabLoc"}
@@ -26,6 +26,6 @@ else
         fi
         cmd+="python3 client_simulator.py --model_name $model_name --test_id ${test_ids[i-1]} --out $outdir --server_address $server_address --server_port $server_port"
     done
-
     eval $cmd
+    echo "Done. You may find logs in $outdir"
 fi

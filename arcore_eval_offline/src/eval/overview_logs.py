15d1947816f6b7c3d935afc49702739e8f749a7c
Samuel Yu <samuelyuyy@gmail.com>
2023-12-09 00:42:15 +0800

    Modify overview_logs.py to draw error distrubution chart

--- arcore_eval_offline/src/eval/overview_logs.py
+++ arcore_eval_offline/src/eval/overview_logs.py
@@ -9,6 +9,9 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', type=Path, required=True)
+parser.add_argument('--display', action='store_true')
+parser.add_argument('--display_dist_req', type=float, default=10)
+parser.add_argument('--display_angle_req', type=float, default=2)
 args = parser.parse_args()
 
 
@@ -56,17 +59,67 @@ def main():
     print('mean', dist_mean, angle_mean)
     print('dist_std', dist_std)
 
-    dists_1std = dists_all[dists_all < dist_mean + 1 * dist_std]
+    # dists_3std = dists_all[dists_all < dist_mean + 3 * dist_std]
     dists_2std = dists_all[dists_all < dist_mean + 2 * dist_std]
-    dists_3std = dists_all[dists_all < dist_mean + 3 * dist_std]
-    angles_1std = angles_all[dists_all < dist_mean + 1 * dist_std]
+    # dists_1std = dists_all[dists_all < dist_mean + 1 * dist_std]
+    # angles_3std = angles_all[dists_all < dist_mean + 3 * dist_std]
     angles_2std = angles_all[dists_all < dist_mean + 2 * dist_std]
-    angles_3std = angles_all[dists_all < dist_mean + 3 * dist_std]
-    print('mean (3 std)', np.mean(dists_3std), np.mean(angles_3std), len(dists_3std), len(dists_3std) / len(dists_all))
+    # angles_1std = angles_all[dists_all < dist_mean + 1 * dist_std]
+    # print('mean (3 std)', np.mean(dists_3std), np.mean(angles_3std), len(dists_3std), len(dists_3std) / len(dists_all))
     print('mean (2 std)', np.mean(dists_2std), np.mean(angles_2std), len(dists_2std), len(dists_2std) / len(dists_all))
-    print('mean (1 std)', np.mean(dists_1std), np.mean(angles_1std), len(dists_1std), len(dists_1std) / len(dists_all))
+    # print('mean (1 std)', np.mean(dists_1std), np.mean(angles_1std), len(dists_1std), len(dists_1std) / len(dists_all))
 
     print('scale', cur_scale)
+
+    if args.display:
+        fig = plt.figure()
+
+        # Draw dist error
+        dists_2std_cm = dists_2std * 100
+        len_dists_2std_cm = len(dists_2std_cm)
+        mean_dists_2std_cm = np.mean(dists_2std_cm)
+        dist_reqirement_cm = args.display_dist_req
+        lower_dists_2std_cm = np.quantile(dists_2std_cm, 0.25)
+        mid_dists_2std_cm = np.quantile(dists_2std_cm, 0.5)
+        higher_dists_2std_cm = np.quantile(dists_2std_cm, 0.75)
+
+        ax = fig.add_subplot(1, 2, 1)
+        ax.figure.set_figwidth(15)
+        ax.set_ylim([0, max(np.quantile(dists_2std_cm, 0.90), higher_dists_2std_cm, mean_dists_2std_cm, dist_reqirement_cm + 5)])
+        ax.hlines(higher_dists_2std_cm, 0, len_dists_2std_cm, 'gray', label=f'75%={higher_dists_2std_cm}cm')
+        ax.hlines(mid_dists_2std_cm, 0, len_dists_2std_cm, 'orange', label=f'50%={mid_dists_2std_cm}cm')
+        ax.hlines(lower_dists_2std_cm, 0, len_dists_2std_cm, 'gray', label=f'25%={lower_dists_2std_cm}cm')
+        ax.hlines(mean_dists_2std_cm, 0, len_dists_2std_cm, 'red', label=f'mean={mean_dists_2std_cm}cm')
+        ax.hlines(dist_reqirement_cm, 0, len_dists_2std_cm, 'green', label=f'requirement={dist_reqirement_cm}cm')
+        ax.plot([i for i in range(len_dists_2std_cm)], np.sort(dists_2std_cm), '.', markersize=0.5, label='Fused pose dist error')
+        ax.legend()
+        ax.set_xlabel('#')
+        ax.set_ylabel('error (cm)')
+        ax.set_title('Dist error')
+
+        # Draw angle error
+        len_angles_2std = len(angles_2std)
+        mean_angles_2std = np.mean(angles_2std)
+        angle_reqirement = args.display_angle_req
+        lower_angles_2std = np.quantile(angles_2std, 0.25)
+        mid_angles_2std = np.quantile(angles_2std, 0.5)
+        higher_angles_2std = np.quantile(angles_2std, 0.75)
+
+        ax = fig.add_subplot(1, 2, 2)
+        ax.set_ylim([0, max(np.quantile(angles_2std, 0.90), higher_angles_2std, mean_angles_2std, angle_reqirement + 1)])
+        ax.hlines(higher_angles_2std, 0, len_angles_2std, 'gray', label=f'75%={higher_angles_2std}deg')
+        ax.hlines(mid_angles_2std, 0, len_angles_2std, 'orange', label=f'50%={mid_angles_2std}deg')
+        ax.hlines(lower_angles_2std, 0, len_angles_2std, 'gray', label=f'25%={lower_angles_2std}deg')
+        ax.hlines(mean_angles_2std, 0, len_angles_2std, 'red', label=f'mean={mean_angles_2std}deg')
+        ax.hlines(2, 0, len_dists_2std_cm, 'green', label='requirement=2deg')
+        ax.plot([i for i in range(len_angles_2std)], np.sort(angles_2std), '.', markersize=0.5, label='Fused pose angle error')
+        ax.legend()
+        ax.set_xlabel('#')
+        ax.set_ylabel('error (deg)')
+        ax.set_title('Angle error')
+
+        plt.draw()
+        plt.show()
 
 
 if __name__ == '__main__':
